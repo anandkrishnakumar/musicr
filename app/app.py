d8bf9f312ddb6fa07f689737ec5bf738bfa328b4
Anand Krishnakumar <anandkrishnakumar123@gmail.com>
2023-12-04 00:45:12 +0000

    webapp stuff

--- app/app.py
+++ app/app.py
@@ -1,98 +1,77 @@
 import streamlit as st
 
 import io
-
-import cv2
-from PIL import Image
-
-import textract
+import math
 
 import numpy as np
 import tensorflow as tf
+import pydub
+import matplotlib.pyplot as plt
+from audiorecorder import audiorecorder
 
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array, load_img, array_to_img
 
-CLASS_IDXS = ["not good", "good"]
-
-@st.cache(allow_output_mutation=True)
+@st.cache_resource
 def load_models():
-  model_eval = load_model('models/doc_model.h5')
-  model_eval._make_predict_function()
-  model_eval.summary()
-  model_auto = load_model('models/auto_model.h5')
-  model_auto._make_predict_function()
-  model_auto.summary()
-  return model_eval, model_auto
+  model_eval = load_model('models/note_detect.h5', compile=False)
+  return model_eval
 
-@st.cache
-def __calculate_score(y_pred_class, y_pred_prob):
-  if y_pred_class == 0:
-    MAX = 0.5
-    scaled_percentage = (y_pred_prob * MAX) / 100
-    return MAX - scaled_percentage
-  else:
-    MAX = 1
-    scaled_percentage = (y_pred_prob * MAX) / 100
-    return scaled_percentage
+def to_array(a, normalized=False):
+    """PyDub to numpy array"""
+    y = np.array(a.get_array_of_samples())
+    if a.channels == 2:
+        y = y.reshape((-1, 2))
+    if normalized:
+        return a.frame_rate, np.float32(y) / 2**15
+    else:
+        return a.frame_rate, y
 
-@st.cache
-def __load_and_preprocess_custom_image(image_path):
-  img = load_img(image_path, color_mode = 'grayscale', target_size = (700, 700))
-  img = img_to_array(img).astype('float32')/255
-  return img
+def freq_to_note(freq):
+    notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
-@st.cache
-def __predict_score(image):
-    image = __load_and_preprocess_custom_image(image)
-    y_pred = model_eval.predict(np.expand_dims(image, axis=0), verbose=1)[0] 
-    y_pred_class = np.argmax(y_pred)
-    y_pred_prob = y_pred[y_pred_class]*100 
-    score = __calculate_score(y_pred_class, y_pred_prob)
-    return y_pred_class, score
+    note_number = 12 * math.log2(freq / 440) + 49  
+    note_number = round(note_number)
+        
+    note = (note_number - 1 ) % len(notes)
+    note = notes[note]
+    
+    octave = (note_number + 8 ) // len(notes)
+    
+    return str(note), str(octave)
 
-@st.cache
-def __auto_encode(image):
-    org_img = load_img(image, color_mode = 'grayscale')
-    org_img = img_to_array(org_img)
-    img = org_img.astype('float32')
-    img = np.expand_dims(img, axis=0)
-    y_pred = np.squeeze(model_auto.predict(img, verbose=1))
-    img = cv2.convertScaleAbs(y_pred, alpha=(255.0))
-    img = Image.fromarray(img)
-    return img
+def get_note(a):
+  print(a.shape)
+  X = np.expand_dims(a, 0)
 
-@st.cache
-def __get_text_from_image(image):
-  text = textract.process(image, method='tesseract', encoding='utf-8')
-  text = text.decode('utf8')
-  return text
+  fig, ax = plt.subplots()
+  ax.plot(X[0])
+  st.pyplot(fig)
 
-st.title("denoise and evaluate images")
-img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
-model_eval, model_auto = load_models()
+  y = model_eval.predict(X)
+  st.write(f"The note is {' '.join(freq_to_note(y))}")
 
-if img_file_buffer is not None:
-    org = load_img(img_file_buffer)
-    y_pred_class, score = __predict_score(img_file_buffer)
-    text = __get_text_from_image(img_file_buffer)
+st.set_page_config(
+        page_title="MUSICR",
+)
 
-    st.image(org, caption=f"Original", width=700)
-    st.write("Predicted class : %s" % (CLASS_IDXS[y_pred_class]))
-    st.write("Score : %f" % (score))
-    st.write(text)
+st.title("Note Detection")
 
-    img = __auto_encode(img_file_buffer)
-    file_object = io.BytesIO()
-    img.save(file_object, 'PNG')
-    y_pred_class, score = __predict_score(file_object)
-    text = __get_text_from_image(file_object)
+st.write("Upload a file or Record")
+audio_buffer = st.file_uploader("Upload an mp3 file", type=["mp3"])
+audio = audiorecorder("Click to record", "Click to stop recording")
 
-    st.image(img, caption=f"Processed Image", width=700)
-    st.write("Predicted class : %s" % (CLASS_IDXS[y_pred_class]))
-    st.write("Score : %f" % (score))
-    st.write(text)
-
-else:
-    st.write('Please upload single image')
+model_eval = load_models()
+if st.button("Find note"):
+    if audio_buffer is not None:
+      audio_read = pydub.AudioSegment.from_file(audio_buffer, "mp3")
+      a = to_array(audio_read, normalized=True)[1][:500, 0]
+      get_note(a)
+    elif len(audio) > 0:
+      # To play audio in frontend:
+      st.audio(audio.export().read())
+      # To get audio properties, use pydub AudioSegment properties:
+      st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
+      a = to_array(audio, normalized=True)[1][400:400+500]
+      a /= np.max(np.abs(a),axis=0)
+      get_note(a)
