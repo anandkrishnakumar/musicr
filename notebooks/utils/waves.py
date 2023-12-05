1930eec4d840ac7be0d64f537373e017ed53a7c2
Anand Krishnakumar <anandkrishnakumar123@gmail.com>
2023-12-05 23:41:31 +0000

    newer model fitting loss fn

--- notebooks/utils/waves.py
+++ notebooks/utils/waves.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 # Generate sine wave
-def gen_sine(freq, sr, secs, noise_sd=False, amplitude=0.5, x_intercept=0):
+def gen_sine(freq, sr, secs, noise_sd=None, amplitude=0.5, x_intercept=0):
     num = int(sr*secs)
-    testing_np_version = np.sin(2 * np.pi * np.arange(num) * freq / sr + x_intercept) * amplitude
-    testing_np_version = np.repeat(testing_np_version[:, np.newaxis], 2, axis=1)
+    testing_np_version = np.zeros((num, 2))
+    testing_np_version[:, 0] = np.sin(2 * np.pi * np.arange(num) * freq / sr + x_intercept) * amplitude
+    testing_np_version[:, 1] = np.sin(2 * np.pi * np.arange(num) * freq / sr + x_intercept) * amplitude
     
     if noise_sd is not None:
         # Add noise
