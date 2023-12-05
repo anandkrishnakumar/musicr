a8f262e3ad953dfb08d5db54b6ff481fcc9200e5
Anand Krishnakumar <anandkrishnakumar123@gmail.com>
2023-12-05 22:07:21 +0000

    optimise gen_sine

--- notebooks/utils/waves.py
+++ notebooks/utils/waves.py
@@ -1,15 +1,13 @@
 import numpy as np
 
 # Generate sine wave
-def gen_sine(freq, sr, secs, noise_sd=False, amplitude=0.5):
+def gen_sine(freq, sr, secs, noise_sd=False, amplitude=0.5, x_intercept=0):
     num = int(sr*secs)
-    testing_np_version = np.zeros((num, 2))
-    testing_np_version[:, 0] = np.sin(2 * np.pi * np.arange(num) * freq / sr) * amplitude
-    testing_np_version[:, 1] = np.sin(2 * np.pi * np.arange(num) * freq / sr) * amplitude
+    testing_np_version = np.sin(2 * np.pi * np.arange(num) * freq / sr + x_intercept) * amplitude
+    testing_np_version = np.repeat(testing_np_version[:, np.newaxis], 2, axis=1)
     
     if noise_sd is not None:
         # Add noise
-        testing_np_version[:, 0] += np.random.normal(0, noise_sd, size=num)
-        testing_np_version[:, 1] += np.random.normal(0, noise_sd, size=num)
+        testing_np_version += np.random.normal(0, noise_sd, size=(num, 2))
 
     return testing_np_version
